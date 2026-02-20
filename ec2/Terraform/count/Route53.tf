805ba09acf839ff3a5e65e0df5bc5d7e45448755
saikumar <saikumar.vundigam@gmail.com>
2026-02-21 04:43:40 +0530

    Terraform actions

--- ec2/Terraform/count/Route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "cloudmine" {
-    count = 10
-  zone_id = var.Hosted_Zone_ID
-  name    = "${var.instances[count.index]}.${var.Domain}" #mongodb.cloudmine.co.in
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.Terraform_Instance[count.index].private_ip]
-}
--- ec2/Terraform/count/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "roboshop_instances" {
-  value = aws_instance.Terraform_Instance
-}
